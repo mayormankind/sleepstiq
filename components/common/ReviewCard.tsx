21cde7eee668496fc9cd6d01c4787d04d3b41ce5
Makinde Mayowa <mayowamakinde23@gmail.com>
2024-07-19 08:32:20 +0100

    Update ReviewCard.tsx

--- components/common/ReviewCard.tsx
+++ components/common/ReviewCard.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 
-export default function ReviewCard({ review, author }) {
+interface reviewProps{
+  review: string;
+  author: string;
+}
+export default function ReviewCard({ review, author }: reviewProps) {
   return (
     <div className='bg-yellowShade rounded-md p-4 w-full max-w-[300px] h-[170px] text-blueShade'>
         <div className='flex flex-col gap-2 h-full justify-between'>

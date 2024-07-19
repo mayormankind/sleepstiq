7f91945bbf0df56b8ce3d7c8de35b30cdb1dcce3
Makinde Mayowa <mayowamakinde23@gmail.com>
2024-07-19 07:22:21 +0100

    Update Mission.tsx

--- components/landing/Mission.tsx
+++ components/landing/Mission.tsx
@@ -6,7 +6,7 @@ export default function Mission() {
     <div className='flex flex-col items-center w-full max-w-5xl py-16 gap-8 px-4 md:px-0 md:flex-row'>
       <div className="flex flex-col my-auto px-4 gap-4 md:px-0 md:pr-12 w-full text-sm md:w-1/2">
         <h2 className='text-xl font-bold'>OUR MISSION</h2>
-        <p>We started Sleepstiq with 1 simple goal: to be your best friend at bedtime. We, just like you, deal with stress, unease, and trouble sleeping from a number of silly things like school, work, screens, numbers, and people. That's why we created products that aim to -</p>
+        <p>We started Sleepstiq with 1 simple goal: to be your best friend at bedtime. We, just like you, deal with stress, unease, and trouble sleeping from a number of silly things like school, work, screens, numbers, and people. That is why we created products that aim to -</p>
         <ul>
           <li>✔ Promote Calm</li>
           <li>✔ Support Sleep</li>
